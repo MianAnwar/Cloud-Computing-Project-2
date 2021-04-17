f4db0392d04357650bba6bb77a5eb10e4faeb1da
MianAnwar <miananwar.2244@gmail.com>
2021-04-17 18:15:49 +0500

    CC Tasks Assign 2 Done

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ShowQ()
+        {
+
+            //We will make a GET request to a really cool website...
+
+            string baseUrl = "http://52.201.152.108/news?id=bcsf17a505";
+            //The 'using' will help to prevent memory leaks.
+            //Create a new instance of HttpClient
+            using (HttpClient client = new HttpClient())
+
+            //Setting up the response...         
+
+            using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+            using (HttpContent content = res.Content)
+            {
+
+                dynamic data = null;
+                data = await content.ReadAsStringAsync();
+                object result = JsonSerializer.Deserialize<List<QuoteModel>>(data);
+                if (data != null)
+                {
+                    return View(result);
+                    //Console.WriteLine(data);
+                }
+            }
+
+
+            return View();
+        }
+
+        public IActionResult LoadQ()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment2/Models/QuoteModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assignment2.Models
+{
+    public class QuoteModel
+    {
+        public string Author{ get; set; }
+        public string Category{ get; set; }
+        public double Popularity{ get; set; }
+        public string Quote { get; set; }
+
+    }
+}
